0fb13b9de09b4e701841936b62b4d7bc8b6f903a
Brenda Ferrufino <ferrufinob@gmail.com>
2021-09-10 16:42:02 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const user = {
+  username: "Brenda",
+  blogs: [
+    {
+      title: "The Best Pumpkin Pie",
+      likes: 50,
+      content: "This is my recipe...",
+    },
+    {
+      title: "The Importance of Sunscreen",
+      likes: 100,
+      content: "Why you should be wearing sunscreen...",
+    },
+  ],
+};
